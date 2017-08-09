9175cc9b4f4add6e53bd29e5309dc7c664f428ea
vijaysai9 <vijaysai.vankadaru@gmail.com>
2017-08-09 16:13:22 -0500

    NEW COMMIT FROM NEW BRANCH

--- src/SampleProject.java
+++ src/SampleProject.java
@@ -4,9 +4,13 @@
 //Main Class
 public class SampleProject {
 //Output statement
+
     public static void main(String[] args){
         //HAI HOW R U DOING
         //System.out.println("Hello World!!!!");
+        System.out.println("welcome to egiants");
+        System.out.println("VIJAY");
+
 
     }
 
