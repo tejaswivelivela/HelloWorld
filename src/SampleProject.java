c9efda5ae174a1a3441842bfbc721d6effecfff0
velivela1193 <velivelatejaswi30@gmail.com>
2017-08-09 14:19:23 -0500

    //Added Comments

--- src/SampleProject.java
+++ src/SampleProject.java
@@ -2,7 +2,7 @@
  * Created by KISH on 8/9/2017.
  */
 public class SampleProject {
-
+//Output statement
     public static void main(String[] args){
         System.out.println("Hello World");
     }
